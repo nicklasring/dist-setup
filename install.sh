d016632d1e6b032124168ff717d956ae01f5f6a4
Nicklas Ring <nicklas@verendus.se>
2023-02-05 22:52:02 +0100

    Initial

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO: Needs to be run as root
+
+apt-get install python3-venv python3-pip ansible sudo
+/usr/sbin/usermod -aG sudo nik
+python3 -m venv ansible-venv
+
+source ansible-venv/bin/activate
+pip install wheel ansible molecule molecule-vagrant
+
+su nik
+
