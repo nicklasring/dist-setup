27d1adaf0c86c255ac7aa9aff7a14bb843ebfc3f
nicklasring <nicklas.ring@verendus.se>
2023-02-19 23:53:26 +0100

    Latest

---
+++ README.md
@@ -0,0 +1,13 @@
+# Debian/Ubuntu setup 
+
+Ansible setup for a debian/ubuntu system, the way i like it.
+
+```
+# Run as root
+./install.sh
+
+# Run as regular user
+ansible-playbook desktop.yml
+```
+
+![Drumpf](img/done.png?raw=true "Youre done")
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# TODO: Needs to be run as root
-
+# Needs to be run as root
 apt-get install python3-venv python3-pip ansible sudo
 /usr/sbin/usermod -aG sudo nik
 python3 -m venv ansible-venv
@@ -10,4 +9,3 @@ source ansible-venv/bin/activate
 pip install wheel ansible molecule molecule-vagrant
 
 su nik
-
